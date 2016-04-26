f8d7ac70f3a42320eee938374225e96959b60b68
Gerard <gerard.han@leandev.se>
2016-04-26 14:07:17 +0800

    add for gina

---
+++ src/ExplorerEncryptionTest.java
@@ -0,0 +1,24 @@
+import java.io.File;
+
+/**
+ * Created by Gerard on 16/4/24.
+ */
+public class ExplorerEncryptionTest {
+    public static void main(String[] args)
+    {
+        String path="document/";
+        File file=new File(path);
+        //定义file类型数组
+        File[] tempList = file.listFiles();
+        System.out.println("该目录下对象个数："+tempList.length);
+        for (int i = 0; i < tempList.length; i++) {
+            if (tempList[i].isFile()) {
+                System.out.println("文     件：" + tempList[i]);
+
+                FileReader.readFileByChars(tempList[i]);
+            }
+        }
+
+    }
+
+}
---
+++ src/FileReader.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+/**
+ * Created by Gerard on 16/4/24.
+ */
+public class FileReader {
+        public static void readFileByChars(File file) {
+        Reader reader = null;
+        try {
+            // 一次读一个字符
+            reader = new InputStreamReader(new FileInputStream(file));
+            int tempchar;
+            while ((tempchar = reader.read()) != -1) {
+                // 对于windows下，\r\n这两个字符在一起时，表示一个换行。
+                // 但如果这两个字符分开显示时，会换两次行。
+                // 因此，屏蔽掉\r，或者屏蔽\n。否则，将会多出很多空行。
+                if (((char) tempchar) != '\r') {
+                    System.out.print((char) tempchar
+                    );
+                }
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+         finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+    }
+
+}
