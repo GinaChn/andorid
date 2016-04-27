058783aa12a11a5722032a37d9eaa702e725e47f
Gerard <gerard.han@leandev.se>
2016-04-27 16:19:52 +0800

    modif git ignore

--- src/ExplorerEncryptionTest.java
+++ src/ExplorerEncryptionTest.java
@@ -2,26 +2,42 @@
  * Copyright (c) 2016 Gina, All rights reserved.
  */
 import java.io.File;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by Gina on 16/4/24.
  */
 public class ExplorerEncryptionTest {
+
+
+
     public static void main(String[] args)
     {
-        String path="document/";
-        File file=new File(path);
-        //定义file类型数组
-        File[] tempList = file.listFiles();
-        System.out.println("该目录下对象个数："+tempList.length);
-        for (int i = 0; i < tempList.length; i++) {
-            if (tempList[i].isFile()) {
-                System.out.println("文     件：" + tempList[i]);
 
-                FileReader.readFileByChars(tempList[i]);
+        String newPath="document/";
+        String oldPath="document_old/";
+
+        Map<String,String> oldFileMap=FileReader.convertFileInfoToMap(oldPath);
+        Map<String,String> newFileMap=FileReader.convertFileInfoToMap(newPath);
+
+        if(oldFileMap.size()!=newFileMap.size())
+        {
+            System.out.println("文件数目不相等!");
+        }
+        Iterator<String> keys = oldFileMap.keySet().iterator();
+        while (keys.hasNext())
+        {
+            String key = (String) keys.next();
+            if(!oldFileMap.get(key).equals(newFileMap.get(key)))
+            {
+                System.out.println("文件:"+key+"内容不相等");
             }
         }
 
     }
+
+
 
 }
