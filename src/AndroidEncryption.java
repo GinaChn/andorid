cba2f6f60650eeebb7db4d2b222f3056652f3337
Gerard <gerard.han@leandev.se>
2016-04-27 16:52:58 +0800

    add for file check

--- src/AndroidEncryption.java
+++ src/AndroidEncryption.java
@@ -10,13 +10,13 @@ public class AndroidEncryption {
 
     public static File encrypt(File file)
     {
-        //please add the encrypt code
+        //用开发的代码来替换这个方法或者调用开发的方法加密文件
         return file;
     }
 
     public static File decrypt(File file)
     {
-        //please add the decrypt code
+        //用开发的代码来替换这个方法或者调用开发的方法解密文件
         return file;
     }
 }
